90e48925cacea03f10d4e2c953b1a7ef968a075b
User Yana <ruavky4@yandex.ru>
2026-06-29 17:27:54 +0300

    fix: resolve checkstyle violations and restore docker-compose services

--- ewm-stats/ewm-stats-client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
+++ ewm-stats/ewm-stats-client/src/main/java/ru/practicum/ewm/stats/client/StatsClient.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 public interface StatsClient {
     void saveHit(EndpointHit hit);
+
     List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 }
